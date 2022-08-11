114fbf7e4f42a56a34929d5823e658b73f210860
Nicolas <nicolascari.rz@gmail.com>
2022-08-11 09:26:07 -0400

    sumador.test

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,7 +1,14 @@
-import sumar from "./sumador.js";
+import {sumar,multiplicar} from "./sumador.js";
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
 });
+
+describe("Multplicar", () => {
+  it("deberia multiplicar dos numeros", () => {
+    expect(multiplicar(3, 2)).toEqual(6);
+  });
+});
+
